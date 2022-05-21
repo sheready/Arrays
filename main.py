e6007de955108a9a39fedd2e8a01e01a3ec5ae8c
Stella Waithera <csstlw@gmail.com>
2022-05-21 20:12:43 +0000

    add the insert method and remove method

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ for x in array1:
 array2 = array('d',[10.2,3.5,7.2,4.5,78.0])
 print(array2[4])
 
+#insert operation to insert one or more elements into an array.
+array3 = array('f',[5.0,2.3,1.5,3.6])
+array3.insert(2,7.8)
+
+for x in array3:
+  print(x)
+
+#deletion refers to removing en element in an array
+#python in-built remove() method
+
+array4 = array('i',[30,20,10,4,3,7])
+array4.remove(4)
+for x in array4:
+  print(x)
