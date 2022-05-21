5c5557c7db66c4b0e08edf95998c18b0c446b4f6
Stella Waithera <csstlw@gmail.com>
2022-05-21 19:54:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#this code shows the different functions that can be performed on an array
+#importing array for array operations
+from array import *
+
+#initialize array with array values
+#The code creates an array named array1
+array1 = array('i',[1,2,3,40,10,80,20,34])
+#the for in loop iterates over every element in a list
+for x in array1:
+  print(x)
+
+#Accessing each element of an array using the index of the element.
+array2 = array('d',[10.2,3.5,7.2,4.5,78.0])
+print(array2[4])
+
